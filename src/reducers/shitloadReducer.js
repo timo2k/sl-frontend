7db504f1c6900931e6a283dd880aa46daeabf8ea
Timo Ischen <timo.ischen@gmail.com>
2019-01-06 14:31:06 +0100

    Shitload reducer fix

--- src/reducers/shitloadReducer.js
+++ src/reducers/shitloadReducer.js
@@ -11,7 +11,7 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				shitloads: action.payload,
-				lodaing: false
+				loading: false
 			};
 		case SHITLOADS_LOADING:
 			return {
